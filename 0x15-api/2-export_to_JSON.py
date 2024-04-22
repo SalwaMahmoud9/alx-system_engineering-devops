cc586f01139618d51ecb60eb497c7796829d1ae8
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-04-22 15:47:37 +0200

    0x15-api Salwa 22-4-2024

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""export to JSON"""
+"""REST API """
+
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    EMPLOYEE_ID = sys.argv[1]
-    USERNAME = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{:d}"
-        .format(int(EMPLOYEE_ID))).json().get("username")
-    ALL_TASKS = []
-    TODOS = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
-    for task in TODOS:
-        if (task.get("userId") == int(EMPLOYEE_ID)):
-            dict = {}
-            dict["task"] = task.get("title")
-            dict["completed"] = task.get("completed")
-            dict["username"] = USERNAME
-            ALL_TASKS.append(list)
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    url += "/" + str(employeeId)
 
-    with open("{}.json".format(EMPLOYEE_ID), 'w') as jsonfile:
-        json.dump({EMPLOYEE_ID: ALL_TASKS}, jsonfile)
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    url += "/todos"
+    response = requests.get(url)
+    tasks = response.json()
+
+    dic = {str(employeeId): []}
+    for t in tasks:
+        dic[str(employeeId)].append({
+            "task": t.get('title'),
+            "completed": t.get('completed'),
+            "username": username
+        })
+    with open('{}.json'.format(str(employeeId)), 'w') as f:
+        json.dump(dic, f)
