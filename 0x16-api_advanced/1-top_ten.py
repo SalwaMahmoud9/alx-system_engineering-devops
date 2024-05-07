9507ef084154e7079eae38e8eb162e17b9863c1a
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-05-07 15:21:42 +0200

    7-5-2024

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,18 +3,21 @@
     posts on Reddit
 """
 
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
     """get first 10 hot post for a subreddit"""
     if subreddit and type(subreddit) is str:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        params = {'limit': 10}
-        req = get(url, params=params, headers=headers, allow_redirects=False)
-        if req.status_code == 200:
-            data = req.json()
+        
+        r = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10}, allow_redirects=False,
+        )
+
+        if r.status_code == 200:
+            data = r.json()
             posts = data.get('data', {}).get('children', {})
             for post in posts:
                 print(post.get('data').get('title'))
