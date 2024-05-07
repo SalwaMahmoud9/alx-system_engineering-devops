b51dc466303e13dffdf1953e98c558f2748f30b2
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-05-07 14:48:18 +0200

    7-5-2024

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,23 +13,7 @@ def count_words(subreddit, word_list, word_count=[], page_after=None):
         for word in word_list:
             word_count.append(0)
 
-    if page_after is None:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        r = get(url, headers=headers, allow_redirects=False)
-        if r.status_code == 200:
-            for child in r.json()['data']['children']:
-                i = 0
-                for i in range(len(word_list)):
-                    for word in [w for w in child['data']['title'].split()]:
-                        word = word.lower()
-                        if word_list[i] == word:
-                            word_count[i] += 1
-                    i += 1
-
-            if r.json()['data']['after'] is not None:
-                count_words(subreddit, word_list,
-                            word_count, r.json()['data']['after'])
-    else:
+    if page_after is not None:
         url = ('https://www.reddit.com/r/{}/hot.json?after={}'
                .format(subreddit,
                        page_after))
@@ -58,3 +42,19 @@ def count_words(subreddit, word_list, word_count=[], page_after=None):
                 for key, value in sorted(dicto.items(),
                                          key=lambda x: (-x[1], x[0])):
                     print('{}: {}'.format(key, value))
+    else:
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+        r = get(url, headers=headers, allow_redirects=False)
+        if r.status_code == 200:
+            for child in r.json()['data']['children']:
+                i = 0
+                for i in range(len(word_list)):
+                    for word in [w for w in child['data']['title'].split()]:
+                        word = word.lower()
+                        if word_list[i] == word:
+                            word_count[i] += 1
+                    i += 1
+
+            if r.json()['data']['after'] is not None:
+                count_words(subreddit, word_list,
+                            word_count, r.json()['data']['after'])
